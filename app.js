b8d860a070f5ea209fd542071fb0312dbf360ef6
Tabor, Kenneth <kenneth.tabor@sabre.com>
2018-03-20 13:25:25 -0500

    world's simplest "Hello World" NodeJS app

---
+++ app.js
@@ -0,0 +1,51 @@
+/* eslint no-underscore-dangle: ["error", { "allow": ["__express"] }] */
+const fs = require('fs');
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const hbs = require('hbs');
+const path = require('path');
+const config = require('./config');
+
+const app = express();
+
+function SetupHandlebars() {
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'html');
+  app.engine('html', hbs.__express);
+}
+
+function SetupLogging() {
+  if (process.env.NODE_ENV !== 'test') {
+    const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'w' });
+    app.use(logger('dev', { stream: accessLogStream }));
+  }
+}
+
+function SetupParsingPublic() {
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(express.static(path.join(__dirname, 'public')));
+}
+
+
+function SetupServer() {
+  SetupHandlebars();
+  SetupLogging();
+  SetupParsingPublic();
+}
+
+function SetupRoutes() {
+  app.get('/', (req, res) => {
+    res.render('main');
+  });
+}
+
+function RunServer() {
+  app.listen(config.port, config.ip, () => {
+    console.log(`WebServer is listening on [${config.ip}][${config.port}] in [${process.env.NODE_ENV}] mode`);
+  });
+}
+
+SetupServer();
+SetupRoutes();
+RunServer();
